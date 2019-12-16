0c6513043082409758ef0e3763f669b225fa1d18
Muhammad Dzakiy <mdzaky93@gmail.com>
2019-12-16 14:41:09 +0700

    Exercise 2 & 3  (kalkulator & looping)

--- main.go
+++ main.go
@@ -57,39 +57,81 @@ import "fmt"
 
 // }
 // --------------------------------------------------------------------------
+// Exercise 1 (+,-,*,/)
+// func main() {
+// 	x := float64(10)
+// 	y := float64(3)
+// 	add := add(x, y)
+// 	min := min(x, y)
+// 	mul := mul(x, y)
+// 	div := div(x, y)
+
+// 	fmt.Println("Nilai x =", x)
+// 	fmt.Println("Nilai y =", y)
+// 	fmt.Println("Penjumlahan ", x, " + ", y, "=", add)
+// 	fmt.Println("Pengurangan ", x, " - ", y, "=", min)
+// 	fmt.Println("Perkalian ", x, " * ", y, "=", mul)
+// 	fmt.Println("Pembagian ", x, " / ", y, "=", div)
+
+// }
+
+// func add(x float64, y float64) float64 {
+// 	add := x + y
+// 	return add
+// }
+
+// func min(x float64, y float64) float64 {
+// 	min := x - y
+// 	return min
+// }
+
+// func mul(x float64, y float64) float64 {
+// 	mul := x * y
+// 	return mul
+// }
+
+// func div(x float64, y float64) float64 {
+// 	div := x / y
+// 	return div
+// }
+// ---------------------------------------------------------------------------------
+// Exercise 2
+// func main() {
+// 	var hasil, x, y float32
+// 	var z string
+// 	fmt.Print("Masukkan bilangan X : ")
+// 	fmt.Scanln(&x)
+// 	fmt.Print("Masukkan Operator : ")
+// 	fmt.Scanln(&z)
+// 	fmt.Print("Masukkan bilangan Y : ")
+// 	fmt.Scanln(&y)
+// 	// hasil = x, z, y
+// 	hasil = perhitungan(x, y, z)
+// 	fmt.Println("Hasil nya adalah", hasil)
+// }
+
+// func perhitungan(x float32, y float32, z string) float32 {
+
+// 	switch z {
+// 	case "+":
+// 		return x + y
+// 		break
+// 	case "-":
+// 		return x - y
+// 		break
+// 	case "*":
+// 		return x * y
+// 		break
+// 	case "/":
+// 		return x / y
+// 	}
+// 	return 0
+// }
+// Exercise 3
 func main() {
-	x := float64(10)
-	y := float64(5)
-	add := add(x, y)
-	min := min(x, y)
-	mul := mul(x, y)
-	div := div(x, y)
+	a := []string{"foo1", "Bar1", "foo2", "Bar2"}
 
-	fmt.Println("Nilai x =", x)
-	fmt.Println("Nilai y =", y)
-	fmt.Println("Penjumlahan ", x, " + ", y, "=", add)
-	fmt.Println("Pengurangan ", x, " - ", y, "=", min)
-	fmt.Println("Perkalian ", x, " * ", y, "=", mul)
-	fmt.Println("Pembagian ", x, " / ", y, "=", div)
-
-}
-
-func add(x float64, y float64) float64 {
-	add := x + y
-	return add
-}
-
-func min(x float64, y float64) float64 {
-	min := x - y
-	return min
-}
-
-func mul(x float64, y float64) float64 {
-	mul := x * y
-	return mul
-}
-
-func div(x float64, y float64) float64 {
-	div := x / y
-	return div
+	for i, s := range a {
+		fmt.Println(i, s)
+	}
 }
